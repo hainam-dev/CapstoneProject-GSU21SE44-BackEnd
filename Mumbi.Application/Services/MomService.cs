681bae2c10aedaf1733365041409cce308d951f9
hainam.dev <hainam210797@gmail.com>
2021-06-11 22:33:43 +0700

    Implement Update Mom API

---
+++ Mumbi.Application/Services/MomService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Mumbi.Application.Dtos.Moms;
+using Mumbi.Application.Interfaces;
+using Mumbi.Application.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mumbi.Application.Services
+{
+    public class MomService : IMomService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MomService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Response<string>> UpdateMomRequest(UpdateMomRequest request)
+        {
+            var mom = await _unitOfWork.MomRepository.FirstAsync(x => x.AccountId == request.AccountId);
+
+            if (mom != null)
+            {
+                mom.AccountId = request.AccountId;
+                mom.FullName = request.FullName;
+                mom.Image = request.Image;
+                mom.Birthday = request.BirthDay;
+                mom.Phonenumber = request.Phonenumber;
+                mom.BloodGroup = request.BloodGroup;
+                mom.RhBloodGroup = request.RhBloodGroup;
+                mom.Weight = request.Weight;
+                mom.Height = request.Height;
+                mom.IsSingleMom = request.IsSingleMom;
+
+
+                _unitOfWork.MomRepository.UpdateAsync(mom);
+                await _unitOfWork.SaveAsync();
+
+                return new Response<string>("Update mom information succesfully", mom.AccountId);
+            }
+
+            return new Response<string>("Update mom information failed");
+        }
+    }
+}
---
+++ Mumbi_API/Controllers/MomsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mumbi.Application.Dtos.Moms;
+using Mumbi.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mumbi_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MomsController : ControllerBase
+    {
+        private readonly IMomService _momService;
+        public MomsController(IMomService momService)
+        {
+            _momService = momService;
+        }
+        [HttpPut("{AccountId}")]
+        public async Task<IActionResult> UpdatePregnancy(string AccountId, UpdateMomRequest request)
+        {
+            if (AccountId != request.AccountId)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _momService.UpdateMomRequest(request));
+        }
+    }
+}
